7d3da71a28b143d9656bf7208c08b021797e629a
medhanarkar <medha_narkar@yahoo.co.in>
2020-08-14 02:38:38 -0700

    first comit of my code

---
+++ src/main/java/com/ta/SeleniumSalesForce/GitHub_2020/App.java
@@ -0,0 +1,13 @@
+package com.ta.SeleniumSalesForce.GitHub_2020;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/ta/SeleniumSalesForce/GitHub_2020/AppTest.java
@@ -0,0 +1,20 @@
+package com.ta.SeleniumSalesForce.GitHub_2020;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
